8f4b3cef9f6eb5308dd194f00454f4929f8f11da
Ulan Urmambetov </>
2025-10-20 16:06:02 +0600

    Initial commit

---
+++ src/main/java/metalabs/metalabstest/MetaLabsTestApplication.java
@@ -0,0 +1,13 @@
+package metalabs.metalabstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaLabsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetaLabsTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/metalabs/metalabstest/MetaLabsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package metalabs.metalabstest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaLabsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
